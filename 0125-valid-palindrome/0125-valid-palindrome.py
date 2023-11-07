b67135fd2673ac4c2c0c3a047af4e3e9744adb31
Qfwfq420 <77083474+Qfwfq420@users.noreply.github.com>
2023-11-07 19:56:17 +0330

    Time: 30 ms (63.39%), Space: 14.8 MB (51.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,6 +5,6 @@ class Solution(object):
         :rtype: bool
         """
         s = ''.join(x for x in s if x.isalpha() or x.isnumeric()).lower()
-        return s == s[::-1]
+        return s[:len(s)//2] == s[(len(s) + 1)//2:][::-1]
 
         
