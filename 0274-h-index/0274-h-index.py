969b9c524c73e7e62c3a6cd73af6921bb57914e4
Qfwfq420 <77083474+Qfwfq420@users.noreply.github.com>
2024-02-14 19:55:59 +0330

    Time: 22 ms (54.11%), Space: 12.1 MB (65.08%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        for i in range(len(citations), -1, -1):
-            cite = [citations[j] >= i for j in range(len(citations))]
-            if cite.count(True) >= i:
-                return i
-        return 0
+        citations.sort(reverse=True)
+        h = 0
+        for i in range(len(citations)):
+            if citations[i] >= i+1: 
+                h = i+1     
+        return h
